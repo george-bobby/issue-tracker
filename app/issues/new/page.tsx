2ed8d465607183f7f9ab4f0aae2d9a70392a5760
George Bobby <mailto.georgebobby@gmail.com>
2024-11-22 18:50:30 +0530

    final

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,16 +1,29 @@
 'use client';
 
-import React from 'react';
-import { TextField, TextArea , Button} from '@radix-ui/themes';
+import React, { useState } from 'react';
+import { TextField, Button } from '@radix-ui/themes';
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+import { useForm, Controller } from "react-hook-form";
+import axios from 'axios';
+import Spinner from '../../Spinner';
+import { useRouter } from 'next/navigation';
+
+interface IssueForm {
+  title: string;
+  description: string;
+ }
 
 const Page = () => {
+  const [submitting, setSubmitting] = useState(false)
+  const router = useRouter()
+  const { register, control, handleSubmit } = useForm<IssueForm>();
   return (
-    <div className='max-w-xl space-y-3'>
-      <TextField.Root placeholder="Title">
-      </TextField.Root>
-      <TextArea placeholder="Reply to comment…" />
-      <Button>Create</Button>
-    </div>
+    <form className='max-w-xl space-y-3' onSubmit={handleSubmit(async data => { setSubmitting(true); await axios.post('/api/issues', data);  router.push('/issues')})}>
+      <TextField.Root placeholder="Title" {...register('title')} />
+      <Controller name='description' control={control} render={ ({field})=> <SimpleMDE {...field} placeholder="Description…" /> } />
+      <Button disabled={ submitting}>Create {submitting &&<Spinner/>}</Button>
+    </form>
   );
 };
 
